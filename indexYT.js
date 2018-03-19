bf03c86fccf7921e1f49087b8c66470644f05f22
Vassilis Angelis <zonev@Vassiliss-iMac.home>
2018-03-19 22:35:39 +0000

    test

--- indexYT.js
+++ indexYT.js
@@ -1,12 +1,15 @@
 let request = require('request');
-let url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=
-AIzaSyB5Sd0e4N-vOWao65BzhyKqt5rmyII8vME&part=snippet,contentDetails,statistics,status`
+let url = 'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=Nmbw4jPomfc&key=AIzaSyB5Sd0e4N-vOWao65BzhyKqt5rmyII8vME'
+//'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&allThreadsRelatedToChannelId=UCSO5KQBCz3GsuDOGLdu7NBw&videoId=zy3Ny5r9uSg&key=AIzaSyB5Sd0e4N-vOWao65BzhyKqt5rmyII8vME'
+//`https://www.googleapis.com/youtube/v3/channels?forUsername='Vassilis Angels'&key=AIzaSyB5Sd0e4N-vOWao65BzhyKqt5rmyII8vME&part=snippet,contentDetails,statistics,status`
+//'videos?id=7lCDEYXw3mM&key=AIzaSyB5Sd0e4N-vOWao65BzhyKqt5rmyII8vME&part=snippet,contentDetails,statistics,status`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let jason = JSON.parse(body);
+    console.log('body:', jason);
   }
 });
 
